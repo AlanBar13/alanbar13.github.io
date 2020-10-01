6ec32d8fe61bec1e1a8db5fc30ec6256e166010a
Alan Gustavo Bardales Mendoza <l03068430@AlanMac.local>
2020-10-01 16:12:56 -0500

    Changed .gitignore

---
+++ build/precache-manifest.e1850f7b800bc53510043c82e15d989d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b066fbe8827538b57bb59040edb9df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed572913028121f51f5",
+    "url": "/static/css/main.c1905d1e.chunk.css"
+  },
+  {
+    "revision": "bd0d5d2647dcc6a01091",
+    "url": "/static/js/2.aa50d3d5.chunk.js"
+  },
+  {
+    "revision": "3ed572913028121f51f5",
+    "url": "/static/js/main.c3223a82.chunk.js"
+  },
+  {
+    "revision": "e1caddb3f18447249b99",
+    "url": "/static/js/runtime~main.148dc81f.js"
+  }
+]);
